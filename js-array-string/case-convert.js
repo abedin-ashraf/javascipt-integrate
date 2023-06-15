2aab53fdcc7ecd81412d624c78170dccdfd6e88b
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2023-06-15 10:41:22 -0500

    first commit

---
+++ js-array-string/case-convert.js
@@ -0,0 +1,9 @@
+const anthem = 'Amar Sonar Bangla Ami Tumai Valobashi';
+const search = 'valo';
+
+const userInput = 'BlackPink';
+const savedUserName = 'blackPink';
+
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+    console.log('user exists');
+}
